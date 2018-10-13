4f2ab9f06fa75fb6863ab8203a389daa557b3730
marko-j <hermarkojovanovic@gmail.com>
2018-10-13 10:22:07 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,25 +3,40 @@
 
 
 function izracunajdane() {
+  
   const satiEkspert = 10000
   const satiPocetnik = 2000
-  brojSatiDnevno=document.getElementById("broj").value;
+  brojSatiDnevno = document.getElementById("broj").value;
   const radnoVreme = 8
   const godina = 365
+  const mesec = 30
 
   const brojDanaPocetnik = satiPocetnik / brojSatiDnevno
   const brojRadnihDana = (satiEkspert - satiPocetnik) / radnoVreme
+  const brojDanaEkspert = brojDanaPocetnik + brojRadnihDana
+
   const brojGodinaPocetnik = brojDanaPocetnik / godina
-  const brojRadnihGodina = brojRadnihDana / godina
-  const brojGodinaEkspert = brojGodinaPocetnik + brojRadnihGodina
+  const brojGodinaEkspert = brojDanaEkspert / godina
 
 
-p1.innerText= ("BROJ GODINA UCENJA DO POCETNIKA JE:") + brojGodinaPocetnik.toFixed()
-p2.innerText= ("BROJ GODINA UCENJA DO EKSPERTA JE:") + brojGodinaEkspert.toFixed()
+  const meseciPocetnik = (brojDanaPocetnik % godina) / mesec
+  const finMeseciPocetnik = Math.floor(meseciPocetnik)
+  const ukupniDaniPocetnik = (brojDanaPocetnik % godina) % mesec
+
+  const meseciEkspert = (brojDanaEkspert % godina) / mesec
+  const finMeseciEkspert = Math.floor(meseciEkspert)
+  const ukupnoDanaEkspert = (brojDanaEkspert % godina) % mesec
+
+
+
+
+
+  p1.innerText = ("VREME POTREBNO DA BUDETE FUNKCIONALNI POCETNIK JE U PROGRAMIRANJU JE:") + brojGodinaPocetnik.toFixed() + "\xa0" + "godina/e" + "\xa0" + "i" + "\xa0" + finMeseciPocetnik + "\xa0" + "meseci" + "\xa0" + "i" + "\xa0" + ukupniDaniPocetnik.toFixed() + "\xa0" + "dana";
+
+  p2.innerText = ("VREME POTREBNO DA BUDETE EKSPERT U PROGRAMIRANJU JE:") + brojGodinaEkspert.toFixed() + "\xa0" + "godina" + "\xa0" + "i" + "\xa0" + finMeseciEkspert + "\xa0" + "meseci" + "\xa0" + "i" + "\xa0" + ukupnoDanaEkspert.toFixed() + "\xa0" + "dana";
 }
-
 const dugme = document.getElementById("gumb")
-dugme.addEventListener("click",izracunajdane);
+dugme.addEventListener("click", izracunajdane);
 
 const p1 = document.getElementById("pasus1")
 const p2 = document.getElementById("pasus2")
