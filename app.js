9cfd1332263503c3406da3e3911dbd663290698b
marko-j <hermarkojovanovic@gmail.com>
2018-10-17 18:06:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,60 @@
 
 
+function izracunajPocetnik() {
 
+  let satiDnevno = document.getElementById("broj").value;
+  return  satiPocetnik / satiDnevno;
 
-function izracunajdane() {
-  
-  const satiEkspert = 10000
-  const satiPocetnik = 2000
-  brojSatiDnevno = document.getElementById("broj").value;
-  const radnoVreme = 8
-  const godina = 365
-  const mesec = 30
-
-  const brojDanaPocetnik = satiPocetnik / brojSatiDnevno
-  const brojRadnihDana = (satiEkspert - satiPocetnik) / radnoVreme
-  const brojDanaEkspert = brojDanaPocetnik + brojRadnihDana
-
-  const brojGodinaPocetnik = brojDanaPocetnik / godina
-  const finGodinaPocetnik = Math.floor (brojGodinaPocetnik)
-  const brojGodinaEkspert = brojDanaEkspert / godina
-  const finGodinaEkspert = Math.floor(brojGodinaEkspert)
-
-
-  const meseciPocetnik = (brojDanaPocetnik % godina) / mesec
-  const finMeseciPocetnik = Math.floor(meseciPocetnik)
-  const ukupniDaniPocetnik = (brojDanaPocetnik % godina) % mesec
-
-  const meseciEkspert = (brojDanaEkspert % godina) / mesec
-  const finMeseciEkspert = Math.floor(meseciEkspert)
-  const ukupnoDanaEkspert = (brojDanaEkspert % godina) % mesec
-
-
-
-
-
-  p1.innerText = ("VREME POTREBNO DA BUDETE FUNKCIONALNI POCETNIK U PROGRAMIRANJU JE:") + finGodinaPocetnik + "\xa0" + "godina/e" + "\xa0" + "i" + "\xa0" + finMeseciPocetnik + "\xa0" + "meseci" + "\xa0" + "i" + "\xa0" + ukupniDaniPocetnik.toFixed() + "\xa0" + "dana";
-
-  p2.innerText = ("VREME POTREBNO DA BUDETE EKSPERT U PROGRAMIRANJU JE:") + finGodinaEkspert + "\xa0" + "godina" + "\xa0" + "i" + "\xa0" + finMeseciEkspert + "\xa0" + "meseci" + "\xa0" + "i" + "\xa0" + ukupnoDanaEkspert.toFixed() + "\xa0" + "dana";
 }
-const dugme = document.getElementById("gumb")
-dugme.addEventListener("click", izracunajdane);
 
-const p1 = document.getElementById("pasus1")
-const p2 = document.getElementById("pasus2")
+function izracunajEkspert() {
+
+  const radnihDana = (satiEkspert - satiPocetnik) / 8;
+  return izracunajPocetnik() + radnihDana;
+
+
+}
+
+
+
+function formatirajIspis(danaPocetnik, danaEkspert) {
+
+  const godinaPocetnik = Math.floor(danaPocetnik / 365);
+  const meseciPocetnik = Math.floor((danaPocetnik % 365) / 30);
+  const ostatakDanaPocetnik = (danaPocetnik % 365) % 30
+
+  const godinaEkspert = Math.floor(danaEkspert / 365)
+  const meseciEkspert = Math.floor((danaEkspert % 365) / 30)
+  const ostatakDanaEkspert = (danaEkspert % 365) % 30
+
+  p1.innerText = `Do funkcionalnog početnika potrebno vam je ${godinaPocetnik} godina, ${meseciPocetnik} mesec/i, ${ostatakDanaPocetnik} dana učenja.`
+  p2.innerText = `Do eksperta vam je potrebno ${godinaEkspert} godina, ${meseciEkspert} mesec/i, ${ostatakDanaEkspert} dana učenja.`
+
+}
+
+function pozivanje() {
+
+  
+  formatirajIspis(izracunajPocetnik(), izracunajEkspert())
+
+}
+
+
+const satiPocetnik = 2000
+const satiEkspert = 10000
+
+
+const p1 = document.getElementById("pasus2")
+const p2 = document.getElementById("pasus3")
+
+const dugme = document.getElementById("gumb")
+dugme.addEventListener("click", pozivanje);
+
+
+
+
+
+
+
+
+
